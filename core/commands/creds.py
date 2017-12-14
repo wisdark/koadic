bdafa987aa8f7e225c9aec27a66bee41f086c6fb
TheNaterz <nate.caroe@risksense.com>
2017-12-14 15:00:18 -0700

    added more info to cred store, added options

--- core/commands/creds.py
+++ core/commands/creds.py
@@ -4,24 +4,50 @@ def autocomplete(shell, line, text, state):
     pass
 
 def help(shell):
-    pass
+    shell.print_plain("")
+    shell.print_plain("Use %s for full credential details" % (shell.colors.colorize("creds -a", shell.colors.BOLD)))
+    shell.print_plain("Use %s for specific user credentials" % (shell.colors.colorize("creds -u user1,user2,user3,...", shell.colors.BOLD)))
+    shell.print_plain("")
 
 def print_creds(shell):
-    nodupes = [dict(tmp) for tmp in set(tuple(item.items()) for item in shell.creds)]
-    shell.creds = nodupes
-    formats = "\t{0:<20}{1:<20}{2:<25}{3:<20}"
-
+    formats = "\t{0:17}{1:<20}{2:<20}{3:<25}{4:<42}{5:<6}"
     shell.print_plain("")
 
-    shell.print_plain(formats.format("USERNAME", "DOMAIN", "PASSWORD", "HASH"))
-    shell.print_plain(formats.format("-"*8,  "-"*6, "-"*8, "-"*4))
+    shell.print_plain(formats.format("IP", "USERNAME", "DOMAIN", "PASSWORD", "HASH", "HASH TYPE"))
+    shell.print_plain(formats.format("--", "-"*8,  "-"*6, "-"*8, "-"*4, "-"*9))
     for cred in shell.creds:
-        shell.print_plain(formats.format(cred["Username"], cred["Domain"], cred["Password"], cred["Hash"]))
+        tmppass = cred["Password"]
+        if len(cred["Password"]) > 23:
+            tmppass = cred["Password"][:20] + "..."
+        shell.print_plain(formats.format(cred["IP"], cred["Username"], cred["Domain"], tmppass, cred["Hash"], cred["HashType"]))
 
     shell.print_plain("")
+
+def print_creds_detailed(shell, users="*"):
+    shell.print_plain("")
+
+    for cred in shell.creds:
+        if users == "*" or cred["Username"].lower() in [u.lower() for u in users.split(",")]:
+            shell.print_plain("IP: "+cred["IP"])
+            shell.print_plain("USERNAME: "+cred["Username"])
+            shell.print_plain("DOMAIN: "+cred["Domain"])
+            shell.print_plain("PASSWORD: "+cred["Password"])
+            shell.print_plain("HASH: "+cred["Hash"])
+            shell.print_plain("HASH TYPE: "+cred["HashType"])
+            shell.print_plain("")
 
 def execute(shell, cmd):
+    nodupes = [dict(tmp) for tmp in set(tuple(item.items()) for item in shell.creds)]
+    shell.creds = nodupes
 
     splitted = cmd.strip().split(" ")
 
-    print_creds(shell)
+    if len(splitted) > 1:
+        if splitted[1] == "-a":
+            print_creds_detailed(shell)
+        elif splitted[1] == "-u":
+            print_creds_detailed(shell, splitted[2])
+        else:
+            shell.print_plain("Error: Unknown option '"+splitted[1]+"'")
+    else:
+        print_creds(shell)
