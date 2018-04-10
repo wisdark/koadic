f7eb2a633b4a3da0e13ec2243d34fa1003fc260f
TheNaterz <nate.caroe@risksense.com>
2018-04-10 13:44:10 -0600

    remove some repeated code, remove killed zombies

--- core/commands/zombies.py
+++ core/commands/zombies.py
@@ -10,17 +10,37 @@ def help(shell):
 
 def execute(shell, cmd):
     splitted = cmd.strip().split(" ")
+    all_sessions = []
+    for stager in shell.stagers:
+        all_sessions.extend(stager.sessions)
+
     if len(splitted) == 1:
-        print_all_sessions(shell)
+        cur_sessions = []
+        for session in all_sessions:
+            if not session.killed:
+                cur_sessions.append(session)
+        print_all_sessions(shell, cur_sessions)
         return
+
+    if splitted[1] == "killed":
+        cur_sessions = []
+        for session in all_sessions:
+            if session.killed:
+                cur_sessions.append(session)
+
+        if len(cur_sessions) == 1:
+            print_session(shell, cur_sessions[0])
+            return
+        elif len(cur_sessions) > 1:
+            print_all_sessions(shell, cur_sessions)
+            return
 
     # Zombie details by IP
     if len(splitted[1].split(".")) == 4:
         cur_sessions = []
-        for stager in shell.stagers:
-            for session in stager.sessions:
-                if session.ip == splitted[1]:
-                    cur_sessions.append(session)
+        for session in all_sessions:
+            if session.ip == splitted[1] and not session.killed:
+                cur_sessions.append(session)
         # If there's only one session, we want to show it
         if len(cur_sessions) == 1:
             print_session(shell, cur_sessions[0])
@@ -34,11 +54,10 @@ def execute(shell, cmd):
         domain_key = [i for i in shell.domain_info if splitted[1].lower() in i][0]
         alt_domain = [i for i in domain_key if i != splitted[1].lower()][0]
         cur_sessions = []
-        for stager in shell.stagers:
-            for session in stager.sessions:
-                d = session.user.split("\\")[0].lower()
-                if d == splitted[1].lower() or d == alt_domain.lower():
-                    cur_sessions.append(session)
+        for session in all_sessions:
+            d = session.user.split("\\")[0].lower()
+            if (d == splitted[1].lower() or d == alt_domain.lower()) and not session.killed:
+                cur_sessions.append(session)
 
         if len(cur_sessions) == 1:
             print_session(shell, cur_sessions[0])
@@ -47,11 +66,10 @@ def execute(shell, cmd):
             print_all_sessions(shell, cur_sessions)
             return
     try:
-        for stager in shell.stagers:
-            for session in stager.sessions:
-                if session.id == int(splitted[1]):
-                    print_session(shell, session)
-                    return
+        for session in all_sessions:
+            if session.id == int(splitted[1]):
+                print_session(shell, session)
+                return
     except ValueError:
         shell.print_error("Expected int or valid ip/domain")
 
@@ -95,24 +113,22 @@ def print_session(shell, session):
     print_jobs(shell, session)
     shell.print_plain("")
 
-def print_all_sessions(shell, specific_sessions=False):
+def print_all_sessions(shell, all_sessions):
     formats = "\t{0:<5}{1:<16}{2:<8}{3:16}"
 
     shell.print_plain("")
 
     shell.print_plain(formats.format("ID", "IP", "STATUS", "LAST SEEN"))
     shell.print_plain(formats.format("---", "---------", "-------", "------------"))
-    for stager in shell.stagers:
-        for session in stager.sessions:
-            alive = "Alive" if session.status == 1 else "Dead"
-            seen = datetime.datetime.fromtimestamp(session.last_active).strftime('%Y-%m-%d %H:%M:%S')
-            elevated = '*' if session.elevated else ''
-            if specific_sessions and session not in specific_sessions:
-                continue
-            shell.print_plain(formats.format(str(session.id)+elevated, session.ip, alive, seen))
+    for session in all_sessions:
+        alive = "Alive" if session.status == 1 else "Dead"
+        seen = datetime.datetime.fromtimestamp(session.last_active).strftime('%Y-%m-%d %H:%M:%S')
+        elevated = '*' if session.elevated else ''
+        shell.print_plain(formats.format(str(session.id)+elevated, session.ip, alive, seen))
 
     shell.print_plain("")
     shell.print_plain('Use "zombies %s" for detailed information about a session.' % shell.colors.colorize("ID", [shell.colors.BOLD]))
     shell.print_plain('Use "zombies %s" for sessions on a particular host.' % shell.colors.colorize("IP", [shell.colors.BOLD]))
     shell.print_plain('Use "zombies %s" for sessions on a particular Windows domain.' % shell.colors.colorize("DOMAIN", [shell.colors.BOLD]))
+    shell.print_plain('Use "zombies killed" for sessions that have been manually killed.')
     shell.print_plain("")
