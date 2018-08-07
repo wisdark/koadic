3b9bdf6baefd7237827419d64d4b956fe8462603
TheNaterz <nate.caroe@risksense.com>
2018-08-07 12:44:05 -0600

    added edit command, edit python/js/vbs files

---
+++ core/commands/edit.py
@@ -0,0 +1,36 @@
+DESCRIPTION = "shell out to an editor for the current module"
+
+def autocomplete(shell, line, text, state):
+    return None
+
+def help(shell):
+    pass
+
+def execute(shell, cmd):
+    import subprocess, os
+
+    if not os.environ['EDITOR']:
+        shell.print_error("$EDITOR env variable not setting, falling back to vi!")
+        editor = 'vi'
+    else:
+        editor = os.environ['EDITOR']
+
+    py_file = "modules/"+shell.state+".py"
+    js_file = "data/"+shell.state+".js"
+    vbs_file = "data/"+shell.state+".vbs"
+
+    splitted = cmd.split()
+
+    if len(splitted) > 1:
+        ftype = splitted[1].lower()
+        if ftype == "py" or ftype == "python":
+            file = py_file
+        elif ftype == "js" or ftype == "javascript":
+            file = js_file
+        elif ftype == "vbs" or ftype == "vbscript":
+            file = vbs_file
+
+        editcmd = [editor, file]
+    else:
+        editcmd = [editor, py_file]
+        subprocess.call(editcmd)
