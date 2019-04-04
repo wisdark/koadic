5b4c09a68917bb639b0a7ce75966e9be4f022ee8
TheNaterz <nate.caroe@risksense.com>
2019-04-04 16:41:52 -0600

    if chinese, try and read hex output, read normal on fail

--- core/job.py
+++ core/job.py
@@ -138,6 +138,16 @@ class Job(object):
 
 
     def decode_downloaded_data(self, data, encoder, text=False):
+        if encoder == "936":
+            try:
+                alldata = data.decode().splitlines()
+                allhex = ""
+                for l in alldata:
+                    allhex += "".join(l.split()[1:17])
+                return bytes.fromhex(allhex)
+            except ValueError:
+                pass
+
         slash_char = chr(92).encode()
         zero_char = chr(0x30).encode()
         null_char = chr(0).encode()
